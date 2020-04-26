e8940e4347d3ecf7887b0374975e7af4a2672d8e
adrianreutter <adrian.reutter@mail.de>
2020-04-26 15:00:19 +0200

    Ändert einige Dinge

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo apt update
-# install nginx with config
-echo "[Step 1] install nginx\n"
+
+step=1
+echo -e "\e[1m[Step $step]\e[21m install nginx\n"
+let "step += 1"
+
 sudo apt install nginx
 sudo cp ./nginx/jenkins.tekwerk-engineering.de /etc/nginx/sites-available/jenkins.tekwerk-engineering.de
 sudo cp ./nginx/git.tekwerk-engineering.de /etc/nginx/sites-available/git.tekwerk-engineering.de
@@ -9,6 +12,8 @@ ln -s /etc/nginx/sites-available/jenkins.tekwerk-engineering.de /etc/nginx/sites
 ln -s /etc/nginx/sites-available/git.tekwerk-engineering.de /etc/nginx/sites-enabled/git.tekwerk-engineering.de
 sudo nginx -t
 
+echo -e "\e[1m[Step $step]\e[21m fix hashbucket memory problem\n"
+let "step += 1"
 # to avoid a possible hash bucket memory problem
 fixhashbucket=server_names_hash_bucket_size; sed -i "s/# $fixhashbucket/$fixhashbucket/g" /etc/nginx/nginx.conf
 sudo systemctl restart nginx
@@ -17,9 +22,34 @@ mkdir /tekwerk/volumes/jenkins
 # fix issue 177 https://github.com/jenkinsci/docker/issues/177
 sudo chown 1000 /tekwerk/volumes/jenkins
 
-sudo groupadd docker
-sudo usermod -aG docker jenkins
+echo -e "\e[1m[Step $step]\e[21m build tekwerk jenkins docker image\n"
+let "step += 1"
 docker build -f Dockerfile.jenkins -t tekwerk/jenkins:latest .
 
-echo "[Step 2] start docker-compose\n"
+echo -e "\e[1m[Step $step]\e[21m start docker-compose\n"
+let "step += 1"
 docker-compose up -d
+
+echo -e "\e[1m[Step $step]\e[21m ssl configuration\n"
+let "step += 1"
+ssl=false
+while true; do
+    read -p "Do you wish to setup certbot?" yn
+    case $yn in
+        [Yy]* ) let "ssl=true"; break;;
+        [Nn]* ) let "ssl=false"; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+if ssl;
+  then
+    sudo apt-get install software-properties-common
+    sudo add-apt-repository universe
+    sudo add-apt-repository ppa:certbot/certbot
+    sudo apt-get update
+    sudo apt-get install certbot python-certbot-nginx
+    sudo certbot --nginx
+fi
+
+echo -e "\e[1m[Done]\e[21m all has been setup\n"
